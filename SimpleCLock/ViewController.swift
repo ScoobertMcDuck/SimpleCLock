846cf67d6ea67af1733488e8a627e0b5b62175e2
ScoobertMcDuck <jamesvarnum@outlook.com>
2024-02-03 12:31:52 -0500

    Initial Commit

---
+++ SimpleCLock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleCLock
+//
+//  Created by Charlotte Norsworthy on 2/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
